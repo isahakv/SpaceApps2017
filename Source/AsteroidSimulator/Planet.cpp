281472603d3fbbbc588279fd4339de698964eb72
DESKTOP-OM18SAU\david <david666.s666@gmail.com>
2017-04-30 11:03:17 +0400

    Full change

---
+++ Source/AsteroidSimulator/Planet.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AsteroidSimulator.h"
+#include "Planet.h"
+
+
+
+
---
+++ Source/AsteroidSimulator/Planet.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "SpaceObject.h"
+#include "Planet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASTEROIDSIMULATOR_API APlanet : public ASpaceObject
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
